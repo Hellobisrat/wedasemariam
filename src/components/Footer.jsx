a8e02d7f29925b4e05e217492362be3771702cd4
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2026-06-16 11:04:33 -0500

    change in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,22 +5,19 @@ import { IoInformationCircle } from "react-icons/io5"
 
 const Footer = () => {
   return (
-    <div className="fixed bottom-0 left-0 right-0 bg-yellow-500 shadow-md p-2 flex justify-around text-gray-700">
+    <div className="fixed bottom-0 left-0 right-0 z-50 bg-yellow-500 shadow-md p-2 flex justify-around text-gray-400">
 
-      {/* HOME */}
-      <Link to="/" className="flex flex-col items-center">
+      <Link to="/" className="flex flex-col items-center active:scale-95 transition-all">
         <AiFillHome className="text-2xl" />
         <span className="text-xs">Home</span>
       </Link>
 
-      {/* PRAYER */}
-      <Link to="/form" className="flex flex-col items-center">
+      <Link to="/form" className="flex flex-col items-center active:scale-95 transition-all">
         <GiPrayer className="text-2xl" />
         <span className="text-xs">Prayer</span>
       </Link>
 
-      {/* INFO */}
-      <Link to="/about" className="flex flex-col items-center">
+      <Link to="/about" className="flex flex-col items-center active:scale-95 transition-all">
         <IoInformationCircle className="text-2xl" />
         <span className="text-xs">Info</span>
       </Link>
