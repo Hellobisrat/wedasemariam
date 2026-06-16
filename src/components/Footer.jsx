06ac5681ac37fed6cf70749953bca2e3e6137c3c
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2026-06-16 11:19:37 -0500

    add cursor to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,21 +3,23 @@ import { AiFillHome } from "react-icons/ai"
 import { GiPrayer } from "react-icons/gi"
 import { IoInformationCircle } from "react-icons/io5"
 
+
 const Footer = () => {
   return (
     <div className="fixed bottom-0 left-0 right-0 z-50 bg-yellow-500 shadow-md p-2 flex justify-around text-gray-400">
 
-      <Link to="/" className="flex flex-col items-center active:scale-95 transition-all">
+      <Link to="/" className="flex flex-col items-center active:scale-95 transition-all cursor-pointer">
         <AiFillHome className="text-2xl" />
         <span className="text-xs">Home</span>
       </Link>
 
-      <Link to="/form" className="flex flex-col items-center active:scale-95 transition-all">
+
+      <Link to="/form" className="flex flex-col items-center active:scale-95 transition-all cursor-pointer">
         <GiPrayer className="text-2xl" />
         <span className="text-xs">Prayer</span>
       </Link>
 
-      <Link to="/about" className="flex flex-col items-center active:scale-95 transition-all">
+      <Link to="/about" className="flex flex-col items-center active:scale-95 transition-all cursor-pointer">
         <IoInformationCircle className="text-2xl" />
         <span className="text-xs">Info</span>
       </Link>
