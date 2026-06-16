dddf36862fdfb3e538797784dc1148600746d4da
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2026-06-16 10:54:52 -0500

    add feature to the link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import { AiFillHome } from "react-icons/ai"
 import { GiPrayer } from "react-icons/gi"
 import { IoInformationCircle } from "react-icons/io5"
-import { Link } from "react-router"
+import { Link } from "react-router-dom";
 
 
 const Footer = () => {
@@ -9,12 +9,17 @@ const Footer = () => {
     <div className="fixed bottom-0 left-0 right-0 bg-yellow-500 shadow-md p-2 flex justify-around text-gray-200">
       <div className="flex flex-col items-center">
         <AiFillHome className="text-2xl" />
-        <Link  to='/' className="text-xs"  >Home</Link>
+        <span className="text-xs">
+        <Link  to='/'   >Home</Link>
+        </span>
       </div>
 
       <div className="flex flex-col items-center">
         <GiPrayer className="text-2xl" />
-        <Link to="/form" className="text-xs">Prayer</Link>
+        <span className="text-xs">
+          <Link to="/form" >Prayer</Link>
+        </span>
+        
       </div>
 
       <div className="flex flex-col items-center">
