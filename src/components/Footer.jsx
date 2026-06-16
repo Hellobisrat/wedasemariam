55d67e1c03c398af9d94d7cb531cbdd76b8ead61
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2026-06-16 10:57:43 -0500

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,31 +1,30 @@
+import { Link } from "react-router-dom"
 import { AiFillHome } from "react-icons/ai"
 import { GiPrayer } from "react-icons/gi"
 import { IoInformationCircle } from "react-icons/io5"
-import { Link } from "react-router-dom";
-
 
 const Footer = () => {
   return (
-    <div className="fixed bottom-0 left-0 right-0 bg-yellow-500 shadow-md p-2 flex justify-around text-gray-200">
-      <div className="flex flex-col items-center">
+    <div className="fixed bottom-0 left-0 right-0 bg-white shadow-md p-2 flex justify-around text-gray-700">
+
+      {/* HOME */}
+      <Link to="/" className="flex flex-col items-center">
         <AiFillHome className="text-2xl" />
-        <span className="text-xs">
-        <Link  to='/'   >Home</Link>
-        </span>
-      </div>
+        <span className="text-xs">Home</span>
+      </Link>
 
-      <div className="flex flex-col items-center">
+      {/* PRAYER */}
+      <Link to="/form" className="flex flex-col items-center">
         <GiPrayer className="text-2xl" />
-        <span className="text-xs">
-          <Link to="/form" >Prayer</Link>
-        </span>
-        
-      </div>
+        <span className="text-xs">Prayer</span>
+      </Link>
 
-      <div className="flex flex-col items-center">
+      {/* INFO */}
+      <Link to="/about" className="flex flex-col items-center">
         <IoInformationCircle className="text-2xl" />
         <span className="text-xs">Info</span>
-      </div>
+      </Link>
+
     </div>
   )
 }
