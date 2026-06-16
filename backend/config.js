90b934aa7515ee78804eca414292ed8688ce46b5
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2026-06-15 19:20:51 -0500

    remove un related directory

--- backend/config.js
+++
@@ -1,3 +0,0 @@
-export const PORT =5555
-export const url='mongodb+srv://zerbisrat:145263@cluster0.tehqspa.mongodb.net/?retryWrites=true&w=majority'
-export const URL ='mongodb+srv://zerbisrat:145263@cluster0.tehqspa.mongodb.net/books-collection?retryWrites=true&w=majority'
